af0c1a4961f57b0714d1f9badeb3687867fd2a30
swarna <lalimanifun@gmail.com>
2025-02-18 17:15:34 +0530

    divisible_by_5.py

--- python/divisible_by_5.py
+++ python/divisible_by_5.py
@@ -1,8 +1,8 @@
 def check_divisibility(num):
-  # // Expected output is "TRUE" or "FALSE"
-  sum = int(input("Enter a number: "))
-print(check_divisibility(sum))
-if sum % 5 == 0:
-   print("TRUE")
-else:
-   print("FALSE")
+    # Check if the number is divisible by 5
+    if num % 5 == 0:
+        return "TRUE"
+    else:
+        return "FALSE"
+num = int(input("Enter a number: "))
+print(check_divisibility(num))
