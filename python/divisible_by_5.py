1adec6f68a368cf532c52fd09b7f0292b8b39d7c
swarnamani0310 <lalimanifun@gmail.com>
2025-02-18 02:35:18 -0800

    Your commit message

--- python/divisible_by_5.py
+++ python/divisible_by_5.py
@@ -1,9 +1,9 @@
 def check_divisibility(num):
-  # // Expected output is "TRUE" or "FALSE"
-
-num = int(input("Enter a number: "))
+  num = int(input("Enter a number: "))
 print(check_divisibility(num))
-if(num % 5 == 0):
+if num % 5 == 0:
   print("True")
-else:
-    print("False")
+  else:
+print("False")
+
+    
